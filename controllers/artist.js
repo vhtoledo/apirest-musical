45ed4bccc927a21c9521519bb3c15a724a8c6ca6
victor toledo <querynetworking@gmail.com>
2023-06-22 03:40:57 -0300

    metodo subir imagen del artista y metodo para mostrar imagen

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,6 +1,8 @@
 // Importaciones 
 const Artist = require("../models/artist");
 const mongoosePagitacion = require("mongoose-pagination");
+const fs = require("fs");
+const path = require("path");
 
 // Metodo Guardar
 const save = (req, res) => {
@@ -134,11 +136,91 @@ const remove = async(req, res) => {
     }
 }
 
+// metodo subir imagen
+const upload = (req, res) => {
+
+    // Recoger artist id
+    let artistId = req.params.id;
+
+    // Recoger el fichero de imagen y comprobar que existe
+    if(!req.file){
+        return res.status(404).send({
+            status: "error",
+            message: "Petición no incluye la imagen"
+        });
+    }
+
+    // Conseguir el nombre del archivo
+    let image = req.file.originalname;
+
+    // Sacar la extension del archivo
+    const imageSplit = image.split("\.");
+    const extension = imageSplit[1];
+    
+    // Comprobar extension
+    if(extension != "png" && extension != "jpg" && extension != "jpeg" && extension != "gif"){
+
+        // Borrar archivo subido
+        const filePath = req.file.path;
+        const fileDeleted = fs.unlinkSync(filePath);
+        // Devolver respuesta negativa
+        return res.status(400).send({
+            status: "error",
+            message: "Extensión del fichero inválida"
+        });
+    }
+
+    // Si es correcta, guardar imagen en base de datos
+    Artist.findByIdAndUpdate({_id: artistId}, {image: req.file.filename}, {new:true})
+        .then((artistUpdate) => {
+            if(!artistUpdate) {
+                return res.status(500).send({
+                    status: "error",
+                    message: "Error en la subida de la imagen"
+                });
+            }
+
+
+            return res.status(200).send({
+                status:"success",
+                user: artistUpdate,
+                file: req.file,
+            });
+        });
+}
+
+// Metodo mostrar avatar
+const image = (req, res) => {
+
+    // Sacar el parametro de la url
+    const file = req.params.file;
+
+    // Montar el path real de la imagen
+    const filePath = "./uploads/artists/"+file;
+
+    // Comprobar que existe
+    fs.stat(filePath, (error, exists) => {
+
+        if(!exists){
+            return res.status(404).send({
+                status: "error", 
+                message: "No existe la imagen"
+            });
+        } 
+        
+        // Devolver un file
+        return res.sendFile(path.resolve(filePath));
+    });
+
+}
+
 // exportar acciones
 module.exports = {
     save,
     one,
     list,
     update,
-    remove
+    remove,
+    upload,
+    image
 }
--- routes/artist.js
+++ routes/artist.js
@@ -8,12 +8,29 @@ const router = express.Router();
 // Importar controlador
 const ArtistController = require("../controllers/artist");
 
+// Configuracion de subida
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads/artists/")
+    },
+    filename: (req, file, cb) => {
+        cb(null, "artist-"+Date.now()+"-"+file.originalname);
+    }
+})
+
+const uploads = multer({storage});
+
+
 // Definir rutas
 router.post("/save", check.auth, ArtistController.save);
 router.get("/one/:id", check.auth, ArtistController.one);
 router.get("/list/:page?", check.auth, ArtistController.list);
 router.put("/update/:id", check.auth, ArtistController.update);
 router.delete("/remove/:id", check.auth, ArtistController.remove);
+router.post("/upload/:id", [check.auth, uploads.single("file0")], ArtistController.upload);
+router.get("/image/:file", ArtistController.image);
 
 // Exportar router
 module.exports = router;
